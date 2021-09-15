337ff3fb291275a0fe68afabb95e5db7b9222b7f
stefanh212 <stefan.huendler2@gmail.com>
2021-09-15 16:28:55 +0200

    added App.js element

--- src/App.js
+++ src/App.js
@@ -6,26 +6,26 @@ class App extends React.Component {
     super(props);
     this.state = {searchText: ''};
   }
-  
+
   return (
     <div className="app">
       <header className="app-header">
         <h1>Search GIPHY</h1>
       </header>
         <div className="searchbar">
-            <input type="search" onChange={this.}/>
+            <input type="search" onChange={e => this.setState({searchText: e.target.value})/>
             <button onClick={searchGiphy}>Search</button>
         </div>
 
         <div className="results">
-            {Array(20).fill(<img src="https://via.placeholder.com/350x350"/>)}
+            {startSearch()}
         </div>
     </div>
   );
 }
-let list = [];
-function searchGiphy() {
-    return await searchGif().map(gif => <img src={gif.url} />)
+
+async function startSearch() {
+    return await searchGif(this.state.searchText).map(gif => <img src={gif.url} />)
 }
 
 export default App;
